9214f3212e7f661a0db2dba18a02d50300d508fc
Tri Pham <skyfallwillfall@gmail.com>
2017-04-19 11:33:18 -0400

    Updated status for enum

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -60,7 +60,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
@@ -68,4 +68,3 @@ class BlogsController < ApplicationController
       params.require(:blog).permit(:title, :body)
     end
 end
-git remote set-url
