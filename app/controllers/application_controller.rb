a851a7bbd3efb2a8863bcf94ec091a420aa705ab
Tri Pham <skyfallwillfall@gmail.com>
2017-04-28 20:08:19 -0400

    installed petergate gem for authroization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = TriViewTool::Renderer.copyright 'Tri Pham', 'All rights reserved'
+  end
+end
+
+
+module TriViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
